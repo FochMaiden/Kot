caf64dd33829cb33be45484cde736005ef9fbc30
Karola <s12592@pjwstk.edu.pl>
2016-06-15 19:56:26 +0200

    trochę kamerki

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,21 @@
 import org.opencv.core.Core;
+import org.opencv.core.Mat;
 import org.opencv.highgui.VideoCapture;
 
-public class Main {
+import javax.swing.*;
+
+public class Main  extends JFrame{
+
+
     public static void main(String[] args) {
 
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
         VideoCapture camera = new VideoCapture(0);
 
+        camera.open(0);
+        if(!camera.isOpened()){
+            System.out.print("Camera error");
+        }
 
     }
 

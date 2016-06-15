de25e4d5f1e333a759ff0c4e1d257447d6fada9a
Karola <s12592@pjwstk.edu.pl>
2016-06-15 19:13:10 +0200

    camera

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,8 @@
-import org.opencv.core.Core;
+import org.opencv.highgui.VideoCapture;
 
 public class Main {
     public static void main(String[] args) {
-       System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-        System.out.println("Hello OpenCV");
-
+        VideoCapture camera = new VideoCapture(0);
     }
 
 }

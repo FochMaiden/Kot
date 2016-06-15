efd4570c5bbe43bcd5eecc29d900f69179f87bec
Blacky <kuba@ruczynski.com>
2016-06-15 19:17:40 +0200

    Loaded library

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,13 @@
+import org.opencv.core.Core;
 import org.opencv.highgui.VideoCapture;
 
 public class Main {
     public static void main(String[] args) {
+
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
         VideoCapture camera = new VideoCapture(0);
+
+
     }
 
 }

e6a004e0f1ff9872d35c0c451dc736557af43d92
Gosia Zdybel <s12593@pjwstk.edu.pl>
2016-06-20 14:27:35 +0200

    new

---
+++ src/main/java/MyFrame.java
@@ -0,0 +1,36 @@
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.highgui.VideoCapture;
+import org.opencv.imgproc.Imgproc;
+
+
+public class MyFrame {
+
+    VideoCapture camera;
+    Mat image,mathelp;
+
+    public MyFrame(VideoCapture camera){
+        this.camera = camera;
+    }
+
+    Mat imageAsBW(){
+        camera.read(image);
+        mathelp = new Mat(image.size(), CvType.CV_8UC1);
+        Imgproc.cvtColor(image,mathelp,Imgproc.COLOR_RGB2GRAY);
+        return mathelp;
+    }
+
+    Mat imageAsHSV(){
+        camera.read(image);
+        mathelp = new Mat();
+        Imgproc.cvtColor(image, mathelp, Imgproc.COLOR_RGB2HSV);
+        return mathelp;
+    }
+
+    Mat imageAsRGB(){
+        camera.read(image);
+        return image;
+    }
+
+
+}
